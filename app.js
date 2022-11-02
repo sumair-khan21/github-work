4c1d1d16c47a14ee548f981a167d5d0e72a3b790
sumair-khan21 <johnalex21210@gmail.com>
2022-11-02 17:31:15 +0500

    code updated

---
+++ app.js
@@ -0,0 +1,36 @@
+// alert("Thanks for your input!");
+
+// var abc = "Mark";
+// window.alert ("abc");
+
+// var nationality; 
+// nationality = "U.S.";
+// alert(nationality);
+
+// var floogle = "mark";
+// var name = "mark";
+// alert(floogle);
+
+// var lessonAuthor = "mark";
+// alert(lessonAuthor);
+
+// var guyWhoKeepsSayingHisOwnName = "mark";
+// alert(guyWhoKeepsSayingHisOwnName);
+
+// var x = "Mark";
+// alert(x);
+
+// var thanx = "Thanks for your input!";
+// alert(thanx);
+
+// var weight = 150;
+// console.log(weight)
+
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+// console.log(newNum)
+
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+console.log(newNum)
